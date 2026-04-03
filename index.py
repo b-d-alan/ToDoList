232c3ec8cd95dff7d0afe6cfc3b98143a3b35d14
b-d-alan <balthasardanielo@gmail.com>
2026-04-03 17:15:14 +0530

    functions created to enable new task creation and printing of pending tasks

--- index.py
+++ index.py
@@ -5,3 +5,13 @@ if "log.csv" not in os.listdir():
     with open("log.csv","w") as f:
         writer = csv.writer(f)
         writer.writerow(["task","status"])
+def new_task(input):
+    with open("log.csv","a") as f:
+        writer= csv.writer(f)
+        writer.writerow([f"{input.split()[1:]}","pending"])
+def show_task():
+    with open("log.csv","r") as f:
+        reader=csv.reader(f)
+        for row in reader:
+            if row[1]=="pending":
+                print(row[0])
